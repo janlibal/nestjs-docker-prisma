c9d1312bdfe9115fa3597e6bf8ebfd1a53dfb660
Jan Libal <libalj@seznam.cz>
2024-12-23 10:50:41 +0100

    Implement logger into operations

--- src/playlist/playlist.controller.ts
+++ src/playlist/playlist.controller.ts
@@ -9,14 +9,16 @@ import {
 import { PlaylistService } from './playlist.service'
 import { Playlist } from './domain/playlist.domain'
 import { PlaylistDto } from './dto/playlist.dto'
+import { PinoLoggerService } from 'src/logger/adapters/pino.logger.service'
 
 @Controller()
 export class PlaylistController {
-  constructor(private readonly playlistService: PlaylistService) {}
+  constructor(private readonly playlistService: PlaylistService, private readonly logger: PinoLoggerService) {this.logger.setContext('PlaylistController')}
 
   @Post('playlist')
   @HttpCode(HttpStatus.OK)
   public insert(@Body() playlistDto: PlaylistDto): Promise<Playlist> {
+    this.logger.log('Parsing data started')
     return this.playlistService.createOne(playlistDto)
   }
 }
--- src/playlist/playlist.service.ts
+++ src/playlist/playlist.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@nestjs/common'
 import { PlaylistDto } from './dto/playlist.dto'
 import { Playlist } from './domain/playlist.domain'
 import { PlaylistRepository } from './playlist.repository'
+import { PinoLoggerService } from 'src/logger/adapters/pino.logger.service'
 
 @Injectable()
 export class PlaylistService {
-  constructor(private playlistRepository: PlaylistRepository) {}
+  constructor(private playlistRepository: PlaylistRepository, private readonly logger: PinoLoggerService) {this.logger.setContext('PlaylistService')}
 
   async createOne(playlistDto: PlaylistDto): Promise<Playlist> {
+    this.logger.log('Processing data started')
     const clonedPayload: Playlist = {
       title: playlistDto.title,
     }
+    this.logger.log('Processing data finished')
     return await this.playlistRepository.save(clonedPayload)
   }
 }
